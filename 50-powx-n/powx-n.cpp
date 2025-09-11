bbed5c38fdfeced91c03ff94e5135a771b5e2e65
Deepak Kumar <75919535+coder-deepak@users.noreply.github.com>
2025-09-12 00:57:30 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (70.88%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if(n == 0){
+            return 1;
+        }
+        if (n < 0) {
+            return 1 / (x * myPow(x, -(n + 1)));
+        }
+        double half = myPow(x, n/2);
+        if(n % 2 == 0){
+            return half * half;
+        }
+        else{
+            return half * half * x;
+        }
+    }
+};
